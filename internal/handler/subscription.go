440c43ac1fd225966d36d918d333df5ca668696a
you@example.com <you@example.com>
2025-12-25 21:44:17 +0500

    feat :The handler layer is fully made

--- internal/handler/subscription.go
+++ internal/handler/subscription.go
@@ -5,7 +5,9 @@ import (
 	"log/slog"
 	"net/http"
 	"strconv"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/mmoldabe-dev/EffectiveTask/internal/domain"
 	"github.com/mmoldabe-dev/EffectiveTask/internal/service"
 )
@@ -101,22 +103,73 @@ func (h *HandlerSubcription) deleteSubscription(w http.ResponseWriter, r *http.R
 }
 func (h *HandlerSubcription) listSubscription(w http.ResponseWriter, r *http.Request) {
 
-	var filter domain.SubscriptionFilter
+	query := r.URL.Query()
 
-	if err := json.NewDecoder(r.Body).Decode(&filter); err != nil {
-		h.log.Error("failed to decode request body", slog.String("error", err.Error()))
-		http.Error(w, "invalid request body", http.StatusBadRequest)
+	userIdStr := query.Get("user_id")
+	userID, err := uuid.Parse(userIdStr)
+
+	if err != nil {
+		http.Error(w, "invalid user_id", http.StatusBadRequest)
+		return
 	}
 
-	subs, err := h.services.List(r.Context(), filter.UserID, filter)
+	limitStr := query.Get("limit")
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil {
+		http.Error(w, "faielt  conv limit", http.StatusBadRequest)
+		return
+	}
+	offsetStr := query.Get("offset")
+	offset, err := strconv.Atoi(offsetStr)
+	if err != nil {
+		http.Error(w, "faielt  conv offset", http.StatusBadRequest)
+		return
+	}
+
+	filter := domain.SubscriptionFilter{
+		UserID: userID,
+		Limit:  limit,
+		Offset: offset,
+	}
+
+	subs, err := h.services.List(r.Context(), userID, filter)
 	if err != nil {
 		h.log.Error("failed to  get List subscriptions ", slog.String("error", err.Error()))
 		http.Error(w, "error", http.StatusNotFound)
+		return
 	}
 
-	w.Header().Set("Content-Type", "applictaion/JSON")
+	w.Header().Set("Content-Type", "applictaion/json")
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(subs); err != nil {
-		h.log.Error("failed to encode response", slog.String("error", err.Error()))
+	json.NewEncoder(w).Encode(subs)
+}
+func (h *HandlerSubcription) extendSubscription(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	idStr := r.PathValue("id")
+	id, err := strconv.ParseInt(idStr, 10, 64)
+	if err != nil {
+		h.log.Error("invalid id parameter", slog.String("id", idStr))
+		http.Error(w, "id must be a number", http.StatusBadRequest)
+		return
 	}
+
+	dateStr := r.URL.Query().Get("new_date")
+	date, err := time.Parse("2006-01-02", dateStr)
+	if err != nil {
+		h.log.Error("invalid date parameter", slog.String("date", dateStr))
+		http.Error(w, "invalid date parameter", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.services.Extend(ctx, id, date); err != nil {
+		h.log.Error("failed to extend subscription", slog.Any("error", err))
+		http.Error(w, "subscription not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"status": "ok",
+	})
 }
