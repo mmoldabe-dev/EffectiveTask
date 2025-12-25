c324bf8aee34c32ba69d291c135bfeb30a6e9e6e
you@example.com <you@example.com>
2025-12-26 00:36:28 +0500

    implement main entry point with graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/mmoldabe-dev/EffectiveTask/internal/config"
+	"github.com/mmoldabe-dev/EffectiveTask/internal/handler"
+	"github.com/mmoldabe-dev/EffectiveTask/internal/repository"
+	"github.com/mmoldabe-dev/EffectiveTask/internal/service"
 	"github.com/mmoldabe-dev/EffectiveTask/internal/storage/postgres"
 	"github.com/mmoldabe-dev/EffectiveTask/pkg/logger"
 )
@@ -13,7 +22,9 @@ func main() {
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		fmt.Printf("Error to load config: %s", err)
+		os.Exit(1)
 	}
+
 	log := logger.SetupLogger(cfg.Logger.Level, "effective_task")
 
 	db, err := postgres.NewPostgres(cfg, log)
@@ -23,5 +34,37 @@ func main() {
 	}
 	defer db.Close()
 
-	log.Info("application is ready to work")
+	repo := repository.NewSubscriptionRepository(db, log)
+	svc := service.NewSubscriptionService(repo, log)
+	handler := handler.NewHandlerSubcription(svc, log)
+
+	router := handler.SetupRouter()
+
+	srv := &http.Server{
+		Addr:         ":" + cfg.Server.Port,
+		Handler:      router,
+		ReadTimeout:  cfg.Server.ReadTimeout,
+		WriteTimeout: cfg.Server.WriteTimeout,
+	}
+
+	go func() {
+		log.Info("starting server", slog.String("port", cfg.Server.Port))
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Error("server error", slog.String("error", err.Error()))
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Info("shutting down server...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("server forced to shutdown", slog.String("error", err.Error()))
+	}
+
+	log.Info("server stopped gracefully")
 }
